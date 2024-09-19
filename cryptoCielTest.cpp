872459468d95f27dce47dd8670088a9a57368398
TFAURE <thomas.fe2005@outlook.com>
2024-09-19 11:55:38 +0200

    Modification pour utiliser AES

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -12,6 +12,28 @@ int main()
 	std::string monMessageHash = "Super message superchouette";
 	std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
 
+	/**
+	 * \brief Generation d'une classe de gestion AES
+	 * 
+	 * \return 
+	 */
+	AesGestion aesGestion;
+	aesGestion.GenerateAESKey();
+
+	//Sauvegarde la clef AES dans un fichier
+	aesGestion.SaveAESKeyToFile("clefAES.txt");
+
+	//Chargement de la clef AES depuis un fichier
+	aesGestion.LoadAESKeyFromFile("clefAES.txt");
+
+	//Chiffre un message
+	aesGestion.EncryptFileAES256("messageDeBase.txt", "messageChiffre.txt");
+
+	//Dechiffre le message
+	aesGestion.DecryptFileAES256("messageChiffre.txt", "messageDechiffre.txt");
+
+	std::cout << LM.CalculateFileSHA256("./messageDechiffre.txt") << std::endl;
+
 }
 
 
